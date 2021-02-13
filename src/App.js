feb1416da4fc14457b740f5571517792ddc826b0
Mani Movassagh <mani.mgh@gmail.comm>
2021-02-13 22:11:50 +0100

    4

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import React from "react";
+import "./App.css";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = { name: "mani programmer" };
+    this.state = { lat: null };
     //update state
-  
-    
+
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({ lat: position.coords.latitude });
+      },
+      (err) => console.log(err)
+    );
   }
 
   render() {
     return (
-      <div>
-        <div>hi mani{this.newname}</div>
+      <div className="container">
+        <p>
+          {this.state.lat > 0 ? "You are in south" : "you are in North"}
+        </p>
       </div>
     );
   }
